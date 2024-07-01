019ae1db72e4dd5a7c46420c5790d4abdda6c926
JARVIS <harshalgupta.dev@gmail.com>
2024-07-01 12:13:17 +0530

    basic Prototype with category sidebar created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,39 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
-
-import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box,Stack, Typography } from '@mui/material'
+import Sidebar from "../components/Sidebar"
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos, setVideos] = useState(null);
-
-  useEffect(() => {
-    setVideos(null);
-
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
-
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
-        </Typography>
-      </Box>
+   <Stack sx={{flexDirection:{sx:"column", md:"row"}}}>
+    <Box px="20px" sx={{height:{sx:"auto",md:"93vh", borderRight:"1px solid #3d3d3d"}}}>
+      <Sidebar></Sidebar>
+      <Typography sx={{color:"#fff", marginTop:"5px"}}>
+        CopyRight 2024 @Dev-Harshal
+      </Typography>
+    </Box>
+   </Stack>
+  )
+}
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-        </Typography>
-
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
-  );
-};
-
-export default Feed;
+export default Feed
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-    </Link>
-    <SearchBar />
+  <Stack direction="row"  p={2} sx={{ position: "sticky", background: '#000', top: 0 }}>
+    <Box sx={{ml:1}}>
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+    </Box>
+    <Box sx={{ margin:"auto", width: {xs:230, sm: "auto"}}} >
+      <SearchBar />
+    </Box>
   </Stack>
 );
 
